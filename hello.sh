c587d4d822599bdd81505111fc64686ebfdbdb75
maruti <maruti.mungi@gmail.com>
2023-06-28 16:30:56 -0400

    deleted last line in featureA

--- hello.sh
+++ hello.sh
@@ -2,4 +2,4 @@ This is a text file in local featureA
 this is to create conflict in feature A origin
 THIS IS TO CREATE SECOND CONFLICT IN FEATUREA.
 this is to create third conflict in featureA.
-this is to create fourth conflict in featureA.
+
