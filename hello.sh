b142c83242d7caccaf80631af35e3196c68d020d
maruti <maruti.mungi@gmail.com>
2023-06-28 16:13:47 -0400

    fix confilict

--- hello.sh
+++ hello.sh
@@ -1,3 +1,5 @@
 This is a text file in local featureA
 this is to create conflict in feature A origin
 THIS IS TO CREATE SECOND CONFLICT IN FEATUREA.
+this is to create third conflict in featureA.
+
